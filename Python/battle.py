03ab14440c729786630820679390f8cd1107b336
Stephen P Duffy <49706638+Sirduffselot@users.noreply.github.com>
2021-07-11 19:12:11 -0500

    Add files via upload

--- Python/battle.py
+++ Python/battle.py
@@ -6,7 +6,7 @@ from guards import Guards
 
 
 
-#Standard battle between two players
+# Standard battle between two players
 class Battle:
     # def __init__(self, Group1, Group2):
     #     Group1 = Group1
@@ -14,20 +14,25 @@ class Battle:
 
     #Battles
     def standardBattle(self, Party1, Party2):
-        #Determines which party leader is faster
+        # Determines whether the battle will continue
+        anyFightersLeft = False
+
+        #Determines which party leader is faster, and thus begins the fight
         partyPrimary, partySecondary = differentiatingSpeed(Party1, Party2)
-        #Initiates battle menu for faster beast
+
+        # Initiates battle menu for faster beast
         battleMenu(partyPrimary, partySecondary)
-        isDead(partyPrimary)
 
-        #Determines which party leader is faster
-        beastPrimary, beastSecondary = differentiatingSpeed(Party1.get_leader(), Party2.get_leader())
-        #Initiates battle menu for slower beast
-        battleMenu(beastPrimary, beastSecondary)
+        # Checks if primary party lost
+        if checkForAnyLivingPartyMembers(partyPrimary) == False:
 
-        #If either party is out of Beasts
-        if (not Party1) or (not Party2):
+            return
 
+        # Initiates battle menu for the other player's beast
+        battleMenu(partySecondary, partyPrimary)
+
+        # Checks if secondary party lost
+        checkForAnyLivingPartyMembers(partySecondary)
 
         return
 
@@ -40,34 +45,65 @@ class Battle:
 
 # The battle menu
 def battleMenu(partyActing, partyOpponent):
-    beastActing = partyActing.get_leader
-    beastOpponent = partyOpponent.get_leader
+    beastActing = partyActing.getLeader
+    beastOpponent = partyOpponent.getLeader
 
     print("What will you do?\n")
     print("Menu:")
     print("1 - Attack")
-    print("2 - Stats")
-    print("3 - Run")
+    print("2 - Spell")
+    print("3 - Stats")
+    print("4 - Run")
 
     userValue = input()
 
     if (userValue == 1):
         attack(beastActing, beastOpponent)
-    elif (userValue == 2):
-        checkStats(beastActing, beastOpponent)
+    elif (userValue == 4):
+        if (isSpellAvailable(partyActing.getLeader()) == True):
+            spell()
     elif (userValue == 3):
+        checkStats(beastActing, beastOpponent)
+        battleMenu(partyActing, partyOpponent)
+    elif (userValue == 4):
         runFromBattle(beastActing, beastOpponent)
     else:
         print("Not a correct option, please try again...")
         battleMenu(partyActing, partyOpponent)
 
+#Checks if the leader of a party has died
+def isBeastDead(party):
+    if party.getLeader().currentHP <= 0:
+        print(party.members[0].getNickname() + " has died...")
+        party.members[0].setCurrentStatus("Dead")
 
+    return
 
-# Individual round between specifically two beasts
+#Swapping Beasts when leader dies
+def swapBeast():
+    #Set new leader
+    #Swap positions of beast in party, dead leader [0] for another living beast
+    return
+
+#Checks a party to see if there are any remaining fighters alive
+def checkForAnyLivingPartyMembers(Party, livingCheck):
+    for x in Party.members:
+        if Party.members[x].currentStatus != "Dead":
+            livingCheck = True
+
+    return livingCheck
+
+#Individual round between specifically two beasts
 def attack(beast1, beast2):
 
+    return
 
+def isSpellAvailable(beast1):
 
+    return
+
+def spell(beast1, beast2):
+    return
 
 def checkStats(beastActing, beastOpponent):
     return
@@ -129,19 +165,3 @@ def luckyValue(beast):
         return 0
 
 
-
-#Swapping Beasts when leader dies
-def swapBeast():
-    return
-
-
-def isDead(party):
-    #Announces party member deaths
-    for x in party:
-        if party.members[x].get_currentHp() is 0:
-            print(party.members[x].get_nickname() + " has died...")
-
-    #Removes dead party members
-    for x in range(2, -1, -1):
-        if party.members[x].get_currentHp() is 0:
-            party.removeDeadFromParty(x)
